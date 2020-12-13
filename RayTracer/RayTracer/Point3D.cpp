e1e4e88b3a6af163ed3e51944197e7b8314afdc2
Guillermo Bernaldo de Quiros Maraver <debugbsd@gmail.com>
2020-12-13 11:16:44 +0100

    - Added Point3D class.

--- RayTracer/RayTracer/Point3D.cpp
+++ RayTracer/RayTracer/Point3D.cpp
@@ -1,9 +1,120 @@
 #include "stdafx.h"
+#include <math.h>
 #include "Point3D.h"
 
+// Default constructor
 Point3D::Point3D(double x, double y, double z)
 {
-	m_Components[0] = x;
-	m_Components[1] = y;
-	m_Components[2] = z;
+	m_Components[POINT3D_X_COMPONENT] = x;
+	m_Components[POINT3D_Y_COMPONENT] = y;
+	m_Components[POINT3D_Z_COMPONENT] = z;
 }
+
+// Copy Constructor
+Point3D::Point3D(const Point3D& other)
+{
+	m_Components[POINT3D_X_COMPONENT] = other.GetX();
+	m_Components[POINT3D_Y_COMPONENT] = other.GetY();
+	m_Components[POINT3D_Z_COMPONENT] = other.GetZ();
+}
+
+// Assignment operator
+Point3D& Point3D::operator=(const Point3D& other)
+{
+	if (this != &other)
+	{
+		m_Components[POINT3D_X_COMPONENT] = other.m_Components[POINT3D_X_COMPONENT];
+		m_Components[POINT3D_Y_COMPONENT] = other.m_Components[POINT3D_Y_COMPONENT];
+		m_Components[POINT3D_Z_COMPONENT] = other.m_Components[POINT3D_Z_COMPONENT];
+	}
+	return *this;
+}
+
+// Subscript operator
+double& Point3D::operator[](int i)
+{
+	if (i >= 0 && i < POINT3D_MAX_COMPONENTS)
+	{
+		return m_Components[i];
+	}
+	else
+	{
+		// We should throw an exception or at least, handle the error...
+		// By default, we return the first element
+		return m_Components[0];
+	}
+}
+
+Point3D Point3D::operator+(const Point3D& other)
+{
+	Point3D t;
+	t.m_Components[POINT3D_X_COMPONENT] = m_Components[POINT3D_X_COMPONENT] + other.m_Components[POINT3D_X_COMPONENT];
+	t.m_Components[POINT3D_Y_COMPONENT] = m_Components[POINT3D_Y_COMPONENT] + other.m_Components[POINT3D_Y_COMPONENT];
+	t.m_Components[POINT3D_Z_COMPONENT] = m_Components[POINT3D_Z_COMPONENT] + other.m_Components[POINT3D_Z_COMPONENT];
+	return t;
+}
+
+Point3D& Point3D::operator+=(const Point3D& other)
+{
+	*this = *this + other;
+	return *this;
+}
+
+Point3D Point3D::operator-(const Point3D& other)
+{
+	Point3D t;
+	t.m_Components[POINT3D_X_COMPONENT] = m_Components[POINT3D_X_COMPONENT] - other.m_Components[POINT3D_X_COMPONENT];
+	t.m_Components[POINT3D_Y_COMPONENT] = m_Components[POINT3D_Y_COMPONENT] - other.m_Components[POINT3D_Y_COMPONENT];
+	t.m_Components[POINT3D_Z_COMPONENT] = m_Components[POINT3D_Z_COMPONENT] - other.m_Components[POINT3D_Z_COMPONENT];
+	return t;
+}
+
+Point3D& Point3D::operator-=(const Point3D& other)
+{
+	*this = *this - other;
+	return *this;
+}
+
+Point3D Point3D::operator*(double a)
+{
+	Point3D t = *this;
+	t.m_Components[POINT3D_X_COMPONENT] *= a;
+	t.m_Components[POINT3D_Y_COMPONENT] *= a;
+	t.m_Components[POINT3D_Z_COMPONENT] *= a;
+	return t;
+}
+
+Point3D& Point3D::operator*=(double a)
+{
+	*this = *this * a;
+	return *this;
+}
+
+Point3D Point3D::operator/(double a)
+{
+	Point3D t = *this;
+	t.m_Components[POINT3D_X_COMPONENT] /= a;
+	t.m_Components[POINT3D_Y_COMPONENT] /= a;
+	t.m_Components[POINT3D_Z_COMPONENT] /= a;
+	return t;
+}
+
+Point3D& Point3D::operator/=(double a)
+{
+	*this = *this / a;
+	return *this;
+}
+
+double Point3D::distance(const Point3D &other) {
+	return (sqrt(
+		(m_Components[POINT3D_X_COMPONENT] - other.m_Components[POINT3D_X_COMPONENT]) * (m_Components[POINT3D_X_COMPONENT] - other.m_Components[POINT3D_X_COMPONENT]) +
+		(m_Components[POINT3D_Y_COMPONENT] - other.m_Components[POINT3D_Y_COMPONENT]) * (m_Components[POINT3D_Y_COMPONENT] - other.m_Components[POINT3D_Y_COMPONENT]) +
+		(m_Components[POINT3D_Z_COMPONENT] - other.m_Components[POINT3D_Z_COMPONENT]) * (m_Components[POINT3D_Z_COMPONENT] - other.m_Components[POINT3D_Z_COMPONENT])));
+}
+double Point3D::squaredDistance(const Point3D&other) {
+	return (
+		(m_Components[POINT3D_X_COMPONENT] - other.m_Components[POINT3D_X_COMPONENT]) * (m_Components[POINT3D_X_COMPONENT] - other.m_Components[POINT3D_X_COMPONENT]) +
+		(m_Components[POINT3D_Y_COMPONENT] - other.m_Components[POINT3D_Y_COMPONENT]) * (m_Components[POINT3D_Y_COMPONENT] - other.m_Components[POINT3D_Y_COMPONENT]) +
+		(m_Components[POINT3D_Z_COMPONENT] - other.m_Components[POINT3D_Z_COMPONENT]) * (m_Components[POINT3D_Z_COMPONENT] - other.m_Components[POINT3D_Z_COMPONENT]));
+}
+
--- RayTracer/RayTracer/Point3D.h
+++ RayTracer/RayTracer/Point3D.h
@@ -1,10 +1,35 @@
 #pragma once
 
+#include "Vector3D.h"
+
+const int POINT3D_X_COMPONENT = 0;
+const int POINT3D_Y_COMPONENT = 1;
+const int POINT3D_Z_COMPONENT = 2;
+const int POINT3D_MAX_COMPONENTS = 3;
+
 // TODO: See 2.7 - Operations (Page 28)
 class Point3D
 {
 public:
-	Point3D(double x, double y, double z);
+	Point3D(double x = 0.0, double y = 0.0, double z = 0.0);		// Default constructor
+	Point3D(const Point3D& other);			// Copy constructor
+
+	Point3D& operator=(const Point3D& other);	// Copy assignment operator
+	double& operator[](int i);					// Subscript operator
+	Point3D operator+(const Point3D& other);	// + operator
+	Point3D& operator+=(const Point3D& other);	// += operator
+	Point3D operator-(const Point3D& other);	// - operator
+	Point3D& operator-=(const Point3D& other);	// -= operator
+	Point3D operator*(double a);				// * operator
+	Point3D& operator*=(double a);				// *= operator
+	Point3D operator/(double a);				// / operator
+	Point3D& operator/=(double a);				// /= operator
+	double distance(const Point3D& other);		// Length
+	double squaredDistance(const Point3D& other);// Length Squared
+
+	double GetX() const { return m_Components[POINT3D_X_COMPONENT]; }
+	double GetY() const { return m_Components[POINT3D_Y_COMPONENT]; }
+	double GetZ() const { return m_Components[POINT3D_Z_COMPONENT]; }
 
 private:
 	double m_Components[3]; // X, Y, Z
